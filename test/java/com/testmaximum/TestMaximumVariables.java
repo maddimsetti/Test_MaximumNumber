076c307ee8a80bc8d61530fa7b3ded1975c3dec5
prudhvi <prudhvieethakota123@gmail.com>
2021-05-18 08:42:38 +0530

    Added: Code for Testing the 2ndPosition MaxNumber in Integers

--- test/java/com/testmaximum/TestMaximumVariables.java
+++ test/java/com/testmaximum/TestMaximumVariables.java
@@ -11,13 +11,19 @@ public class TestMaximumVariables {
     MaximumVariables maximum = new MaximumVariables();      //creating object for passing Test Cases
 
     /**
-     * @description create method for Testing the Three Integers which is maximum
-     * param: Maximum Number at First Position should return the same Number
+     * @description creating methods for Testing the Three Integers which is maximum
+     * param1: Maximum Number at First Position should return the same Number
+     * param1: Maximum Number at Second Position should return the same Number
      */
     @Test
     public void givenThreeNumbers_1stPosition_shouldReturnMaximum() {
         Integer result = maximum.maximumNUmber(9,3,4);
         Assertions.assertEquals(9, result);
+    }
+    @Test
+    public void givenThreeNumbers_2ndPosition_shouldReturnMaximum() {
+        Integer result = maximum.maximumNUmber(9,10,4);
+        Assertions.assertEquals(10, result);
     }
 
 }
